c5b9dc8aad331ee7574dd399eafdc81c8767c2fa
Shubham Jain <shubhamkjain@outlook.com>
2018-04-23 19:30:42 +0530

    undo removing DB config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,7 +2,14 @@ const firebase = require('firebase');
 // Required for side-effects
 require('firebase/firestore');
 
-console.log(config);
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+};
 
 firebase.initializeApp(config);
 
@@ -12,18 +19,18 @@ export { firebase, db as default };
 
 // // const demoData = db.collection('Demo').doc('demoDoc');
 //
-// const expenses = db.collection('expenses');
+const expenses = db.collection('expenses');
+
+expenses.add({
+  description: 'Dosa',
+  note: '',
+  amount: 109500,
+}).then(() => {
+  console.log('Added the record');
+}).catch((err) => {
+  console.log(err);
+});
 //
-// expenses.add({
-//   description: 'Dosa',
-//   note: '',
-//   amount: 109500,
-// }).then(() => {
-//   console.log('Added the record');
-// }).catch((err) => {
-//   console.log(err);
-// });
-// //
 // expenses.add({
 //   description: 'Phone bill',
 //   note: '',
